976ae8ea7ad551b7c9c6872a790d349e313a9ce3
Jack Clark <chubz@Jacks-MacBook-Air.local>
2017-06-10 11:50:50 -0500

    Initial Commit. Not much is going on atm.

---
+++ TabViewTesting/Bar.swift
@@ -0,0 +1,21 @@
+//
+//  Bar.swift
+//  TabViewTesting
+//
+//  Created by Jack Clark on 6/10/17.
+//  Copyright © 2017 Jack Clark. All rights reserved.
+//
+
+import UIKit
+
+class Bar: UITabBar {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+    
+}
---
+++ TabViewTesting/TabViewController.swift
@@ -0,0 +1,36 @@
+//
+//  TabViewController.swift
+//  TabViewTesting
+//
+//  Created by Jack Clark on 6/10/17.
+//  Copyright © 2017 Jack Clark. All rights reserved.
+//
+
+import UIKit
+
+class TabViewController: UITabBarController {
+    @IBOutlet weak var TabBar: UITabBar!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
